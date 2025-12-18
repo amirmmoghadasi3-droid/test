4e07270ddd31794b9fab0328b09a065b62101e0c
Amir Mohammad <amirmmoghadasi3@gmail.com>
2025-12-19 00:07:44 +0330

    Update amir_mohamad_moghadasi_4-1.cpp

--- amir_mohamad_moghadasi_4-1.cpp
+++ amir_mohamad_moghadasi_4-1.cpp
@@ -1,24 +1,60 @@
-//امیر محمد مقدسی 4-1
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
-int factorer(int);
-int main()
- {
-    cout<<"please enter a number:"<<endl;
-    int number;
-    cin>>number;
-    if (number>=0)
-    //n!=n(n-1)!
-    cout <<endl<<factorer(number);
-    else
-        cout <<endl<<number;
- return 0;
- }
-// فانکشن فاکتوریر
-int factorer(int number)
- {
-    if(number==0)
-        return 1;
-    else
-        return number*factorer(number-1);
- }
+
+int gamevalue[25];
+bool opened[25] = {false};
+int score = 0;
+
+void gamebord() {
+    cout << "+----+----+----+----+----+\n";
+    for (int i = 0; i < 25; i++) {
+        if (opened[i])
+            cout << "| " << gamevalue[i] << "  ";
+        else {
+            if (i+1 < 10)
+                cout << "| 0" << i+1 << " ";
+            else
+                cout << "| " << i+1 << " ";
+        }
+
+        if ((i+1) % 5 == 0) {
+            cout << "|\n+----+----+----+----+----+\n";
+        }
+    }
+}
+
+void clear() {
+    cout << "\033[2J\033[1;1H";
+}
+
+int main() {
+    srand(time(NULL));
+
+    for(int i = 0; i < 25; i++)
+        gamevalue[i] = 1 + rand() % 9;  // امتیاز هر خانه
+
+    while (true) {
+        gamebord();
+        cout << "Score: " << score << endl;
+        cout << "Select a box (1-25, 0 to exit): ";
+
+        int choice;
+        cin >> choice;
+
+        if (choice == 0) break;
+        if (choice < 1 || choice > 25 || opened[choice-1]) {
+            cout << "Invalid choice!\n";
+            continue;
+        }
+
+        opened[choice-1] = true;
+        score += gamevalue[choice-1];
+
+        clear();
+    }
+
+    cout << "Final Score: " << score << endl;
+    return 0;
+}
